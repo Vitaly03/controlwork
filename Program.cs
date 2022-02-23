a51734efa6d72972c468f49e26b48e74ceb34f54
Vitaly <Vega.tishenko@yandex.ru>
2022-02-23 18:09:44 +0300

    Добавил Блок схему

---
+++ Program.cs
@@ -0,0 +1,35 @@
+string[] array = {"Hello", "2", "world", ":-)"};
+
+string[] newarray (string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    string[] newarray = new string [count];
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newarray[j] = array[i];
+            j++;
+        }
+    }
+    return newarray;
+}
+
+void PrintArray(string[] array)           
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine();
+}
+PrintArray(array);
+PrintArray(newarray(array));
